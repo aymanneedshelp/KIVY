aba9b2ca9f27f3a30b8d2082f8c203f273f41977
Ayman Khan <ayman02khan@gmail.com>
2021-06-14 10:02:01 +0530

    added buttons to box layout

--- thelab/main.py
+++ thelab/main.py
@@ -1,5 +1,16 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+
+class BoxLayoutExample(BoxLayout):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        b1 = Button(text="A")
+        b2 = Button(text="B")
+        self.add_widget(b1)
+        self.add_widget(b2)
 
 class MainWidget(Widget):
     pass
