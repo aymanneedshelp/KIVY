ae29a4f5dcf09d8e8f76a9b365f76a159e801de7
Ayman Khan <ayman02khan@gmail.com>
2021-06-14 12:39:49 +0530

    added anchor layout

--- thelab/main.py
+++ thelab/main.py
@@ -1,7 +1,11 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.button import Button
+
+class AnchorLayoutExample(AnchorLayout):
+    pass
 
 class BoxLayoutExample(BoxLayout):
 
