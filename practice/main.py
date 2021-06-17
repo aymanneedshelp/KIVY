2db460b24ea4f9c5920a2e064ee39f1dd57709ac
Ayman Khan <ayman02khan@gmail.com>
2021-06-17 19:37:09 +0530

    Made nested layouts

--- practice/main.py
+++ practice/main.py
@@ -2,11 +2,18 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.stacklayout import StackLayout
+from kivy.uix.pagelayout import PageLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.anchorlayout import AnchorLayout
 
-
-
-class MainBoxLayout(BoxLayout):
-    pass
+class MainStackLayout(StackLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        b = Button(text="Home",size_hint=(.1,.1))
+        b2 = Button(text="TITLE",size_hint=(.9,.1))
+        self.add_widget(b)
+        self.add_widget(b2)
 
 class PracticeApp(App):
     pass
