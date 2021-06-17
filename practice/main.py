4eba92dda423e6467eaaf4c419d0171af5dddcd8
Ayman Khan <ayman02khan@gmail.com>
2021-06-17 11:21:22 +0530

    tried box layout

---
+++ practice/main.py
@@ -0,0 +1,14 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+
+
+
+class MainBoxLayout(BoxLayout):
+    pass
+
+class PracticeApp(App):
+    pass
+
+PracticeApp().run()
