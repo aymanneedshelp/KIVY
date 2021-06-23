bace921761692721c9fd783662ff1e4309ca4d38
Ayman Khan <ayman02khan@gmail.com>
2021-06-23 11:58:40 +0530

    Added button through the py file

--- thelab/main.py
+++ thelab/main.py
@@ -6,7 +6,10 @@ from kivy.uix.button import Button
 from kivy.uix.stacklayout import StackLayout
 
 class StackLayoutExample(StackLayout):
-    pass
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        b = Button(text='G',size_hint=(.2,.2))
+        self.add_widget(b)
 
 class AnchorLayoutExample(AnchorLayout):
     pass
