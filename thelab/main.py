6b7ae7e65a38740a1a368332c0e68223b071a3cc
Ayman Khan <ayman02khan@gmail.com>
2021-06-23 12:21:02 +0530

    added 100 buttons and a scrollview

--- thelab/main.py
+++ thelab/main.py
@@ -1,4 +1,5 @@
 from kivy.app import App
+from kivy.metrics import dp
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.anchorlayout import AnchorLayout
@@ -8,8 +9,8 @@ from kivy.uix.stacklayout import StackLayout
 class StackLayoutExample(StackLayout):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
-        for i in range(10):
-            b = Button(text=str(i+1),size_hint=(.2,.2))
+        for i in range(100):
+            b = Button(text=str(i+1),size_hint=(None,None), size=(dp(100),dp(100)))
             self.add_widget(b)
 
 class AnchorLayoutExample(AnchorLayout):
