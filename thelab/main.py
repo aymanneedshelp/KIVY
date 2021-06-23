d6f577c994337210c41741c075ffb8fb231c6601
Ayman Khan <ayman02khan@gmail.com>
2021-06-23 12:02:49 +0530

    created buttons using loop

--- thelab/main.py
+++ thelab/main.py
@@ -8,8 +8,9 @@ from kivy.uix.stacklayout import StackLayout
 class StackLayoutExample(StackLayout):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
-        b = Button(text='G',size_hint=(.2,.2))
-        self.add_widget(b)
+        for i in range(10):
+            b = Button(text=str(i+1),size_hint=(.2,.2))
+            self.add_widget(b)
 
 class AnchorLayoutExample(AnchorLayout):
     pass
